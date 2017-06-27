b04f472469edbea15758b75bf9bbc521228b7815
KEVIN R BURNHAM <kb@KEVINs-MacBook-Pro.local>
2017-06-27 12:09:53 -0500

    add bd_songs and bd_concerts data sets

--- R/data.R
+++ R/data.R
@@ -12,3 +12,33 @@
 #'   }
 #' @source \url{http://www.bobdylan.com/}
 "bd_songs"
+
+
+#' Names, year of publication, song, and song position for each Bob Dylan Album
+#'
+#' @format A data.frame with 1064 rows and 4 columns
+#' \describe{
+#'   \item{Albums}{Album name}
+#'   \item{Year}{Year of publication}
+#'   \item{Song}{Name of Song}
+#'   \item{Position}{Ordinal position of song on album}
+#'   }
+#'  @source \url{http://www.bobdylan.com/}
+"bd_albums"
+
+
+#' Information about time, place and set list for Bob Dylan Concerts
+#'
+#' @format A data.frame with 54542 rows and 7 columns
+#' \describe{
+#'   \item{Date}{Date of concert}
+#'   \item{Venue}{Name of venue}
+#'   \item{City}{City location of venue}
+#'   \item{State/Country}{The state (in in the USA) or country where venue is located}
+#'   \item{Songs}{The name of a song played at the concert}
+#'   \item{Position}{The ordinal position of the song in the set list}
+#'   \item{OriginalLocation}{The unseparated City/State(Country) from the raw data}
+#'   }
+#' @source \url{http://www.bobdylan.com/}
+"bd_concerts"
+
